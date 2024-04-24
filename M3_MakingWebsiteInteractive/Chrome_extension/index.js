c7b82b7db5222935204452f89a5084f2f06fe5fb
roche-tan <rochelyn.rallestan@gmail.com>
2024-04-24 19:26:45 +0200

    updated chrome extension project

--- M3_MakingWebsiteInteractive/Chrome_extension/index.js
+++ M3_MakingWebsiteInteractive/Chrome_extension/index.js
@@ -1,15 +1,36 @@
-// let myLeads = [];
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
+let myLeads = [];
+// let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
-
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  console.log("Button clicked");
-  // console.log(myLeads);
+  renderLead();
+  inputEl.value = "";
 });
 
-for (let i = 0; i < myLeads.length; i++) {
-  console.log(myLeads[i]);
+// function renderLeads() {
+//   let listItems = "";
+//   for (let i = 0; i < myLeads.length; i++) {
+//     // ulEl.innerHTML += `<li>${myLeads[i]}</li>`;
+//     /* const li = document.createElement("li");
+//     li.textContent = myLeads[i]
+//     ulEl.append(li)
+//     */
+//     listItems += `<li>${myLeads[i]}</li>`;
+//   }
+//   ulEl.innerHTML = listItems;
+// }
+
+// more eficient method
+function renderLead() {
+  let listItem = `
+    <li>
+      <a href="${inputEl.value}" target="_blank">
+        ${inputEl.value}
+      </a>
+    </li>`;
+  ulEl.innerHTML += listItem;
+  console.log(listItem);
 }
