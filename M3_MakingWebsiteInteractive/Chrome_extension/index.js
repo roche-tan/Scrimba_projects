f5fa4418d6d5893869c6785126a89d78b2fb0057
roche-tan <rochelyn.rallestan@gmail.com>
2024-04-25 13:44:33 +0200

    finised chrome extension

--- M3_MakingWebsiteInteractive/Chrome_extension/index.js
+++ M3_MakingWebsiteInteractive/Chrome_extension/index.js
@@ -3,34 +3,47 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const tabBtn = document.getElementById("tab-btn");
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
+});
+
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+          <li>
+              <a target='_blank' href='${leads[i]}'>
+                  ${leads[i]}
+              </a>
+          </li>
+      `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+deleteBtn.addEventListener("dblclick", function () {
+  // console.log("double click");
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  renderLead();
   inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
-
-// function renderLeads() {
-//   let listItems = "";
-//   for (let i = 0; i < myLeads.length; i++) {
-//     // ulEl.innerHTML += `<li>${myLeads[i]}</li>`;
-//     /* const li = document.createElement("li");
-//     li.textContent = myLeads[i]
-//     ulEl.append(li)
-//     */
-//     listItems += `<li>${myLeads[i]}</li>`;
-//   }
-//   ulEl.innerHTML = listItems;
-// }
-
-// more eficient method
-function renderLead() {
-  let listItem = `
-    <li>
-      <a href="${inputEl.value}" target="_blank">
-        ${inputEl.value}
-      </a>
-    </li>`;
-  ulEl.innerHTML += listItem;
-  console.log(listItem);
-}
